1df01fd2bf640d0f4797e7fb864e64ea4b883076
dranelol <cardsfan853@gmail.com>
2014-02-21 23:15:56 -0600

    Revert "better update"
    This reverts commit 168cee4efb121f7ea0942ea9d86556f7d055d7a6.

--- vTrakr.lua
+++ vTrakr.lua
@@ -3,20 +3,16 @@ updateInterval = 1.0; -- How often the OnUpdate code will run (in seconds)
 
 -- Functions Section
 function vTrakr_OnUpdate(self, elapsed)
-  local time = GetTime();
-  -- self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed; 	
+  self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed; 	
 
-  if (self.TimeSinceLastUpdate < time) then
+  if (self.TimeSinceLastUpdate > updateInterval) then
     print("im trackin dat v"); 
 
-    self.TimeSinceLastUpdate = time + updateInterval;
+    self.TimeSinceLastUpdate = 0;
   end
 end
 
 
 function vTrakr_OnLoad() 
-  self.TimeSinceLastUpdate = 0;
-  self.RegisterForDrag("LeftButton");
-
   print("vTrakr Loaded"); 
 end
