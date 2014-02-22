168cee4efb121f7ea0942ea9d86556f7d055d7a6
dranelol <cardsfan853@gmail.com>
2014-02-21 23:15:52 -0600

    better update

--- vTrakr.lua
+++ vTrakr.lua
@@ -3,16 +3,20 @@ updateInterval = 1.0; -- How often the OnUpdate code will run (in seconds)
 
 -- Functions Section
 function vTrakr_OnUpdate(self, elapsed)
-  self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed; 	
+  local time = GetTime();
+  -- self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed; 	
 
-  if (self.TimeSinceLastUpdate > updateInterval) then
+  if (self.TimeSinceLastUpdate < time) then
     print("im trackin dat v"); 
 
-    self.TimeSinceLastUpdate = 0;
+    self.TimeSinceLastUpdate = time + updateInterval;
   end
 end
 
 
 function vTrakr_OnLoad() 
+  self.TimeSinceLastUpdate = 0;
+  self.RegisterForDrag("LeftButton");
+
   print("vTrakr Loaded"); 
 end
